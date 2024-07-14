4daec878fe9ce212da84b01ad5bce664ed7076c7
Pulkit <pulkit.singh@journeyfront.com>
2024-07-14 13:25:09 +0530

    fixed error states

--- app.vue
+++ app.vue
@@ -3,43 +3,10 @@
     <nuxt-page />
   </nuxt-layout>
 </template>
-<script>
-import "@/assets/style/style.css";
+<script setup>
+import '@/assets/style/style.css';
+import { categories } from '@/data/categories';
 
-export default {
-  name: "App",
-  head: {
-    title: "Elden Ring Fan Wiki",
-    meta: [
-      {
-        hid: "description",
-        name: "description",
-        content: "Elden Ring Fan API"
-      }
-    ],
-    link: [
-      {
-        rel: "icon",
-        type: "image/x-icon",
-        href: "/favicon.ico"
-      },
-      {
-        rel: "preconnect",
-        href: "https://fonts.googleapis.com"
-      },
-      {
-        rel: "preconnect",
-        href: "https://fonts.gstatic.com",
-        crossorigin: true
-      },
-      {
-        href: "https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap",
-        rel: "stylesheet"
-      }
-    ]
-  },
-  setup() {
-    const categories = useState([]);
-  }
-};
+const cats = useState('categories', () => [...categories]);
+
 </script>
