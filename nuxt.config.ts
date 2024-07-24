fa087c27f224d9ae59b6575fed33feed38aee5e9
Pulkit <pulkit.singh@journeyfront.com>
2024-07-24 16:39:36 +0530

    Updated app title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 import vitePluginRequire from "vite-plugin-require";
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+title: 'Elden Ring Wiki'
+    }
+  },
   modules: ['@nuxtjs/tailwindcss', "@nuxt/icon"],
   compatibilityDate: '2024-07-14',
   vite: {
